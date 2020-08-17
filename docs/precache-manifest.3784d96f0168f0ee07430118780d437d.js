17e75d1bdc80a3d7670c320c6d76f7e7f59707ff
Aleksey <nirall@mail.ru>
2020-08-17 10:42:38 +0500

    For production

---
+++ docs/precache-manifest.3784d96f0168f0ee07430118780d437d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9c7634c192210fdcebc667f0f14d182",
+    "url": "/avt/index.html"
+  },
+  {
+    "revision": "ea59b3ab98928789b6fe",
+    "url": "/avt/static/css/main.cbd2d933.chunk.css"
+  },
+  {
+    "revision": "a5bda1d8e47e3614b423",
+    "url": "/avt/static/js/2.1f44a51b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/avt/static/js/2.1f44a51b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea59b3ab98928789b6fe",
+    "url": "/avt/static/js/main.e1624eb7.chunk.js"
+  },
+  {
+    "revision": "ca194a3de2fa089617e1",
+    "url": "/avt/static/js/runtime-main.9e23d4c7.js"
+  },
+  {
+    "revision": "112bbf51bbb15038cd56d0d6087b230c",
+    "url": "/avt/static/media/logo.112bbf51.jpg"
+  },
+  {
+    "revision": "fb0a14140344e70b30892534e314721b",
+    "url": "/avt/static/media/main-bg.fb0a1414.jpg"
+  }
+]);
