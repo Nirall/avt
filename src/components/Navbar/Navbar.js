3df80c19ed4929158b63702011e1ed32dd64675c
Aleksey <nirall@mail.ru>
2020-08-20 23:29:29 +0500

    Navbar is responsive

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -8,21 +8,25 @@ function Navbar(props) {
   const navList = URLs.map((url) => {
     return <NavbarLink path = { url.path } name = { url.name } key = { url.name }/>
   });
+
+  const burgerHandler = () => {
+    document.querySelector(".navbar__list").classList.toggle("navbar__list_visible");
+  }
+
   return (
     <nav className = "navbar">
+      <div className = "navbar__burger" onClick = { burgerHandler }>&#9776;</div>
       <div className = "navbar__item">
         <img src = { logo } className = 'logo' alt = 'логотип' width = "130px"/>
       </div>
-      <div className = "navbar__item">
-        <div className = 'navbar__list'>
+      <div className = "navbar__item navbar__list">
           { navList }
-        </div>
       </div>
-      <div className = "navbar__item">
+      <div className = "navbar__item navbar__contacts">
         <a href = 'tel:+79655485500' className = "navbar__link">+7 965 548-55-00</a>
         <a href = 'mailto:avt-technology@mail.ru' className = "navbar__link">avt&#8209;technology@mail.ru</a>
       </div>
-      <div className = "navbar__item">
+      <div className = "navbar__item navbar__buttons">
         <button className = "navbar__button">Заказать звонок</button>
         <button className = "navbar__button">Отправить заявку</button>
       </div>
